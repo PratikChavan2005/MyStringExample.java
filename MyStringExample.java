d0e7a61bfd100f4445abe25273963119f7bf72fc
PratikChavan2005 <pratikchavan963744@gmail.com>
2024-10-30 22:57:39 +0530

    Add files via upload

---
+++ MyStringExample.java
@@ -0,0 +1,9 @@
+class MyStringExample {
+    
+    public static void main(String[] args) {
+        System.out.println("Let's learn about Strings");
+        
+        String name = "MY NAME IS ABC";
+        System.out.println(name);
+    }
+}
